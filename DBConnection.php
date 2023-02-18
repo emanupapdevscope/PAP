af12e71d9cf750346d379d8124db3419b2441e2d
Emanuel Oliveira <emanueloliveira.163511@etpc.pt>
2023-02-18 13:24:01 +0000

    t

--- DBConnection.php
+++ DBConnection.php
@@ -1,6 +1,6 @@
 	
 	<?php
-	$linkk = mysqli_init();
+	$link = mysqli_init();
 mysqli_ssl_set($link,NULL,NULL, 'ca.pem', NULL, NULL);
 mysqli_real_connect($link, "papemanu.mysql.database.azure.com", "emanu", "P@ssword1", "pap", 3306, MYSQLI_CLIENT_SSL);
 
