ddaacd1ebdac679d0e8a638d4768747da0ec2dda
Emanuel Oliveira <emanueloliveira.163511@etpc.pt>
2023-02-18 14:10:42 +0000

    Update logout.php

--- logout.php
+++ logout.php
@@ -5,9 +5,9 @@ session_start();
 
 //destroi as variaveis
 
-unset($_SESSION[iduser]);
-unset($_SESSION[user]);
-unset($_SESSION[tipo]);
+unset($_SESSION['iduser']);
+unset($_SESSION['user']);
+unset($_SESSION['tipo']);
 session_destroy();
 
 //redireciona para a tela de login
