7b4e3b360808d5c31c5357b155250715cfd709fc
Emanuel Oliveira <emanueloliveira.163511@etpc.pt>
2023-02-22 11:36:11 +0000

    Update registarleite.php

--- registarleite.php
+++ registarleite.php
@@ -1,12 +1,5 @@
  <?php
 session_start();
-
-if($_SESSION['tipo'] == 1){
-}
-else{
-	Header("refresh:0.1;url=user.php");}
-
-	
 	$link = mysqli_init();
 	mysqli_ssl_set($link,NULL,NULL, 'ca.pem', NULL, NULL);
 	mysqli_real_connect($link, "papemanu.mysql.database.azure.com", "emanu", "P@ssword1", "pap", 3306, MYSQLI_CLIENT_SSL);
