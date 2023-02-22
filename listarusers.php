f04d9134dc359758a1d555ecadd3d9ddbceb5840
Emanuel Oliveira <emanueloliveira.163511@etpc.pt>
2023-02-22 09:59:13 +0000

    Update listarusers.php

--- listarusers.php
+++ listarusers.php
@@ -1,5 +1,21 @@
 
-<?php include "sessaoseguraadmin.php";?>
+<?php require 'vendor/autoload.php';
+use Predis\Client;
+$redis = new Predis\Client([
+    'scheme' => 'tcp',
+    'host'   => 'emanu.redis.cache.windows.net',
+    'port'   => 6380,
+    'password' => 'dF2qyIAwXjJUPlRpfhGcnNnWQqPOyAoKKAzCaJhJWho='
+]);
+
+session_start();
+if($_SESSION['tipo'] == 1){
+    $_SESSION['iduser']=$_SESSION['iduser'];
+	$_SESSION['user']=$_SESSION['user'];
+	$_SESSION['tipo'] = $_SESSION['tipo'];
+}
+else{
+	Header("refresh:0.1;url=user.php");}?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
